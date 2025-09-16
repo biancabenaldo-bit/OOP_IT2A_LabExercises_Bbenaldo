740e45657706e2599080b318c5add55881ecdcfa
biancabenaldo-bit <biancabenaldo@gmail.com>
2025-09-16 13:34:54 +0800

    Create EXER2_Car.java

---
+++ EXER2_Benaldo/EXER2_Car.java
@@ -0,0 +1,66 @@
+public class Car {
+    private String color;
+    private String plateNo;
+    private String chassisNo;
+    private String brand;
+    private String origin;
+    private double price;
+
+    // No-argument constructor
+    public Car() {
+        this.color = "No Color";
+        this.plateNo = "No PlateNumber";
+        this.chassisNo = "No Chassis No Yet!";
+        this.brand = "Unknown";
+        this.origin = "Unknown";
+        this.price = 0.0;
+    }
+
+    // constructor
+    public Car(String color, String plateNo, String chassisNo,
+               String brand, String origin, double price) {
+        this.color = color;
+        this.plateNo = plateNo;
+        this.chassisNo = chassisNo;
+        this.brand = brand;
+        this.origin = origin;
+        this.price = price;
+    }
+
+    
+    public void displayInfo() {
+        String info = "";
+        info += "Brand: " + this.brand;
+        info += "\nColor: " + this.color;
+        info += "\nPlateNo: " + this.plateNo;
+        info += "\nChassisNo: " + this.chassisNo;
+        info += "\nOrigin: " + this.origin;
+        info += "\nPrice: " + this.price;
+
+        System.out.println(info);
+        System.out.println("-----------------------------");
+    }
+}
+
+public class CarTester {
+    public static void main(String[] args) {
+        
+        Car[] cars = new Car[10]; //10 CARS
+
+        cars[0] = new Car("Black", "BMW123", "CH001", "BMW", "Germany", 6000000);
+        cars[1] = new Car("White", "CAD456", "CH002", "Cadillac", "USA", 7500000);
+        cars[2] = new Car("Silver", "CHR789", "CH003", "Chrysler", "USA", 3500000);
+        cars[3] = new Car("Blue", "ALP111", "CH004", "Alpina", "Germany", 9000000);
+        cars[4] = new Car("Gray", "AUD222", "CH005", "Audi", "Germany", 5000000);
+        cars[5] = new Car("Green", "BEN333", "CH006", "Bentley", "UK", 12000000);
+        cars[6] = new Car("Red", "BRA444", "CH007", "Brabus", "Germany", 15000000);
+        cars[7] = new Car("Orange", "CHE555", "CH008", "Chery", "China", 1200000);
+        cars[8] = new Car("Yellow", "TES666", "CH009", "Tesla", "USA", 8000000);
+        cars[9] = new Car("Purple", "LAM777", "CH010", "Lamborghini", "Italy", 20000000);
+
+       
+        for (Car car : cars) {
+            car.displayInfo();
+        }
+    }
+      }
