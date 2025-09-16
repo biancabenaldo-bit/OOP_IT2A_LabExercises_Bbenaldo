c3673a5dbbdeb0cb8ba87f123999e61c5d0d0374
biancabenaldo-bit <biancabenaldo@gmail.com>
2025-09-16 13:27:37 +0800

    Create EXER1_Imperative.java

---
+++ EXER1_Benaldo/EXER1_Imperative.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 70, 50};
+        Int sum = 0;
+
+        for (int I = 0; I < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println(“Imperative Sum = “ + sum);
+    }
+}
+
+Imperative Sum= 180
+
+
+
+
+
+
+
