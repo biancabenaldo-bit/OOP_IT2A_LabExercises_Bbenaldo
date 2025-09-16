dc8d06b044d864a07c9d7984404b41676516bc7b
biancabenaldo-bit <biancabenaldo@gmail.com>
2025-09-16 13:25:16 +0800

    Create EXER1_Declarative.java

---
+++ EXER1_Benaldo/EXER1_Declarative.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 70, 50};
+
+
+        int sum = Arrays.stream(numbers).sum();
+
+
+        System.out.println(“Declarative Sum = “ + sum);
+
+    }
+
+}
+
+
+
+Declarative Sum= 180
+
